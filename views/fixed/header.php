c01b613443bce0cfbfa532733f1b94c45265ab88
Andrej <zarke998@gmail.com>
2020-06-03 19:40:51 +0200

    Change logo

--- views/fixed/header.php
+++ views/fixed/header.php
@@ -47,7 +47,7 @@
                             <!-- Logo -->
                             <div class="col-xl-1 col-lg-1 col-md-1 col-sm-3">
                                 <div class="logo">
-                                  <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                                  <a href="index.html"><img src="assets/img/logo2.png" alt=""></a>
                                 </div>
                             </div>
                             <div class="col-xl-6 col-lg-8 col-md-7 col-sm-5">
