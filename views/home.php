4b1070fa0174f91e00b23f395957599865c2bf16
Andrej Zarkovski <zarke998@gmail.com>
2020-06-04 17:20:06 +0200

    Change home newsletter area style

--- views/home.php
+++ views/home.php
@@ -47,7 +47,7 @@
         </section>
         <!-- Category Area End-->
         <!-- Latest Products Start -->
-        <section class="latest-product-area padding-bottom">
+        <section class="latest-product-area pb-3">
             <div class="container-fluid px-5">
                 <div class="row product-btn d-flex align-items-end">
                     <!-- Section Tittle -->
@@ -253,117 +253,26 @@
             </div>
         </section>
         <!-- Latest Products End -->
-        <!-- Best Product Start -->
-        <div class="best-product-area lf-padding" >
-           <div class="product-wrapper bg-height" style="background-image: url('assets/img/categori/card.png')">
-                <div class="container position-relative">
-                    <div class="row justify-content-between align-items-end">
-                        <div class="product-man position-absolute  d-none d-lg-block">
-                            <img src="assets/img/categori/card-man.png" alt="">
-                        </div>
-                        <div class="col-xl-2 col-lg-2 col-md-2 d-none d-lg-block">
-                            <div class="vertical-text">
-                                <span>Manz</span>
-                            </div>
-                        </div>
-                        <div class="col-xl-8 col-lg-8">
-                            <div class="best-product-caption">
-                                <h2>Find The Best Product<br> from Our Shop</h2>
-                                <p>Designers who are interesten creating state ofthe.</p>
-                                <a href="#" class="black-btn">Shop Now</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-           </div>
-           <!-- Shape -->
-           <div class="shape bounce-animate d-none d-md-block">
-               <img src="assets/img/categori/card-shape.png" alt="">
-           </div>
-        </div>
-        <!-- Best Product End-->
-        <!-- Best Collection Start -->
-        <div class="best-collection-area section-padding2">
-            <div class="container">
-                <div class="row d-flex justify-content-between align-items-end">
-                    <!-- Left content -->
-                    <div class="col-xl-4 col-lg-4 col-md-6">
-                        <div class="best-left-cap">
-                            <h2>Best Collection of This Month</h2>
-                            <p>Designers who are interesten crea.</p>
-                            <a href="#" class="btn shop1-btn">Shop Now</a>
-                        </div>
-                        <div class="best-left-img mb-30 d-none d-sm-block">
-                            <img src="assets/img/collection/collection1.png" alt="">
-                        </div>
-                    </div>
-                    <!-- Mid Img -->
-                     <div class="col-xl-2 col-lg-2 d-none d-lg-block">
-                        <div class="best-mid-img mb-30 ">
-                            <img src="assets/img/collection/collection2.png" alt="">
-                        </div>
-                    </div>
-                    <!-- Riht Caption -->
-                    <div class="col-xl-4 col-lg-6 col-md-6">
-                        <div class="best-right-cap ">
-                           <div class="best-single mb-30">
-                               <div class="single-cap">
-                                   <h4>Menz Winter<br> Jacket</h4>
-                               </div>
-                               <div class="single-img">
-                                  <img src="assets/img/collection/collection3.png" alt="">
-                               </div>
-                           </div>
-                        </div>
-                        <div class="best-right-cap">
-                           <div class="best-single mb-30">
-                               <div class="single-cap active">
-                                   <h4>Menz Winter<br>Jacket</h4>
-                               </div>
-                               <div class="single-img">
-                                  <img src="assets/img/collection/collection4.png" alt="">
-                               </div>
-                           </div>
-                        </div>
-                        <div class="best-right-cap">
-                           <div class="best-single mb-30">
-                               <div class="single-cap">
-                                   <h4>Menz Winter<br> Jacket</h4>
-                               </div>
-                               <div class="single-img">
-                                  <img src="assets/img/collection/collection5.png" alt="">
-                               </div>
-                           </div>
-                        </div>
-                    </div> 
-                </div>
-            </div>
-        </div>
-        <!-- Best Collection End -->
         <!-- Latest Offers Start -->
         <div class="latest-wrapper lf-padding">
-            <div class="latest-area latest-height d-flex align-items-center" data-background="assets/img/collection/latest-offer.png">
+            <div id="newsletter-area" class="latest-area latest-height d-flex align-items-center">
                 <div class="container">
                     <div class="row d-flex align-items-center">
                         <div class="col-xl-5 col-lg-5 col-md-6 offset-xl-1 offset-lg-1">
                             <div class="latest-caption">
-                                <h2>Get Our<br>Latest Offers News</h2>
-                                <p>Subscribe news latter</p>
+                                <h2 class="text-stroke-white-1">Get Our<br>Latest Offers News</h2>
+                                <p>Subscribe to the newsletter</p>
                             </div>
                         </div>
                          <div class="col-xl-5 col-lg-5 col-md-6 ">
                             <div class="latest-subscribe">
                                 <form action="#">
                                     <input type="email" placeholder="Your email here">
-                                    <button>Shop Now</button>
+                                    <button>Subscribe</button>
                                 </form>
                             </div>
                         </div>
                     </div>
-                </div>
-                <!-- man Shape -->
-                <div class="man-shape">
-                    <img src="assets/img/collection/latest-man.png" alt="">
                 </div>
             </div>
         </div>
