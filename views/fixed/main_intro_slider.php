e92402af494c1d3b2f245c3305107d8779769133
Andrej <zarke998@gmail.com>
2020-06-04 12:11:36 +0200

    Change home slider layout

--- views/fixed/main_intro_slider.php
+++ views/fixed/main_intro_slider.php
@@ -1,7 +1,29 @@
 <div class="slider-area ">
             <!-- Mobile Menu -->
             <div class="slider-active">
-                <div class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">
+                <div id="main-intro-slider" class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">
+                    <div class="container">
+                        <div class="row d-flex justify-content-between pt-8 pb-5">
+                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
+                                <div id="main-intro-slider-image" class="hero__img" data-animation="fadeIn" data-delay=".4s">
+                                    <img src="assets/images/book-cover-1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-xl-5 col-lg-5 col-md-5 col-sm-8 mt-5">
+                                <div id="main-intro-slider-info" class="hero__caption">
+                                    <span data-animation="fadeInRight" data-delay=".4s">60% Discount</span>
+                                    <h1 class="text-stroke-black"data-animation="fadeInRight" data-delay=".6s">Heart Spring Mountain</h1>
+                                    <p class="mt-5" data-animation="fadeInRight" data-delay=".8s">Heart Spring Mountain” explores the buried emotions that surface when a woman, Vale, returns to her hometown after her drug-addicted mother, Bonnie, disappears in a hurricane. The story slowly weaves through several generations of the same family, exposing how the past influences the present</p>
+                                    <!-- Hero-btn -->
+                                    <div class="hero__btn" data-animation="fadeInRight" data-delay="1s">
+                                        <a href="industries.html" class="btn hero-btn">Shop Now</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">
                     <div class="container">
                         <div class="row d-flex align-items-center justify-content-between">
                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
@@ -14,7 +36,7 @@
                                     <span data-animation="fadeInRight" data-delay=".4s">60% Discount</span>
                                     <h1 data-animation="fadeInRight" data-delay=".6s">Winter <br> Collection</h1>
                                     <p data-animation="fadeInRight" data-delay=".8s">Best Cloth Collection By 2020!</p>
-                                    <!-- Hero-btn -->
+                                     Hero-btn 
                                     <div class="hero__btn" data-animation="fadeInRight" data-delay="1s">
                                         <a href="industries.html" class="btn hero-btn">Shop Now</a>
                                     </div>
@@ -22,28 +44,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="single-slider slider-height" data-background="assets/img/hero/h1_hero.jpg">
-                    <div class="container">
-                        <div class="row d-flex align-items-center justify-content-between">
-                            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
-                                <div class="hero__img" data-animation="bounceIn" data-delay=".4s">
-                                    <img src="assets/img/hero/hero_man.png" alt="">
-                                </div>
-                            </div>
-                            <div class="col-xl-5 col-lg-5 col-md-5 col-sm-8">
-                                <div class="hero__caption">
-                                    <span data-animation="fadeInRight" data-delay=".4s">60% Discount</span>
-                                    <h1 data-animation="fadeInRight" data-delay=".6s">Winter <br> Collection</h1>
-                                    <p data-animation="fadeInRight" data-delay=".8s">Best Cloth Collection By 2020!</p>
-                                    <!-- Hero-btn -->
-                                    <div class="hero__btn" data-animation="fadeInRight" data-delay="1s">
-                                        <a href="industries.html" class="btn hero-btn">Shop Now</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                </div> -->
             </div>
         </div>
