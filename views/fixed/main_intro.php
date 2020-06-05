421c8dc57446c8a861581a7262cc364d000b0cf9
Andrej <zarke998@gmail.com>
2020-06-05 15:44:33 +0200

    Change shop main intro style

--- views/fixed/main_intro.php
+++ views/fixed/main_intro.php
@@ -1,14 +1,14 @@
 <?php 
     function getMainIntro($title){
         return '
-        <div class="slider-area ">
+        <div id="main-intro" class="slider-area ">
         <!-- Mobile Menu -->
-            <div class="single-slider slider-height2 d-flex align-items-center" data-background="assets/img/hero/category.jpg">
+            <div class="single-slider slider-height2 d-flex align-items-center">
                 <div class="container">
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="hero-cap text-center">
-                                <h2>'.$title.'</h2>
+                                <h2 class="text-stroke-black-2">'.$title.'</h2>
                             </div>
                         </div>
                     </div>
