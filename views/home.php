674da0264c29ec521fd040ec63966746620b2d8e
Andrej <zarke998@gmail.com>
2020-06-05 12:42:41 +0200

    Extract bullet points section to a fixed view

--- views/home.php
+++ views/home.php
@@ -1,3 +1,7 @@
+<?php 
+    define("ROOT",$_SERVER["DOCUMENT_ROOT"]);
+?>
+
 <main>
 
         <!-- Category Area Start-->
@@ -277,35 +281,9 @@
             </div>
         </div>
         <!-- Latest Offers End -->
-        <!-- Shop Method Start-->
-        <div class="shop-method-area section-padding30 py-5">
-            <div class="container">
-                <div class="row d-flex justify-content-between">
-                    <div class="col-xl-3 col-lg-3 col-md-6">
-                        <div class="single-method mb-40">
-                            <i class="ti-package"></i>
-                            <h6>Shipping Worldwide</h6>
-                            <p>For purchases over 50$ free shiping to your home address.(Check country eligibility)</p>
-                        </div>
-                    </div>
-                    <div class="col-xl-3 col-lg-3 col-md-6">
-                        <div class="single-method mb-40">
-                            <i class="ti-unlock"></i>
-                            <h6>Free books in pdf</h6>
-                            <p>Sign up for an account, and gain access to numerous free eBooks in pdf.</p>
-                        </div>
-                    </div> 
-                    <div class="col-xl-3 col-lg-3 col-md-6">
-                        <div class="single-method mb-40">
-                            <i class="ti-reload"></i>
-                            <h6>Recycle used books</h6>
-                            <p>By sending us used books for recycling, you get 25% discount on your next purchase.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Shop Method End-->
+        <?php
+            include ROOT."/views/fixed/bullet_points.php";
+        ?>
         <!-- Gallery Start-->
         <div id="best-by-critics">
             <div class="section-tittle mb-30 ml-4">
