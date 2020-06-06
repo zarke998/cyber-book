58255e848393b48452738baf7eaf3e82bd45a0be
Andrej Zarkovski <zarke998@gmail.com>
2020-06-06 19:38:45 +0200

    Add config

---
+++ config/config.php
@@ -0,0 +1,26 @@
+<?php
+    define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
+
+    define("ENV_FILE", ROOT."/config/.env");
+    define("SEPARATOR", "&");
+
+    //DB Credentials
+    define("SERVER", env("SERVER"));
+    define("DBNAME", env("DBNAME"));
+    define("USERNAME", env("USERNAME"));
+    define("PASSWORD", env("PASSWORD"));
+    
+    function env($key){
+        $keyValuePairs = file(ENV_FILE);
+        foreach($keyValuePairs as $pair){
+            $data = explode("=", $pair);
+            if($data[0] == $key)
+                if(count($data) == 2)
+                    return $data[1];
+                else
+                    return "";
+        }
+    }
+
+    echo SERVER. " " .DBNAME. " ". USERNAME. " ". PASSWORD;
+?>
