02211c233e696869cc3be2499cf3eac15939e229
Andrej Zarkovski <zarke998@gmail.com>
2020-06-06 19:52:18 +0200

    Add connection file

---
+++ config/connection.php
@@ -0,0 +1,16 @@
+<?php 
+    define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
+
+    require_once ROOT."/config/config.php";
+
+    try{
+        $dbname = DBNAME;
+        $host = SERVER;
+        $conn = new PDO("mysql:dbname=$dbname;host=$host", USERNAME, PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    }
+    catch(Exception $e){
+        echo "Error connecting to database.";
+    }
+?>
