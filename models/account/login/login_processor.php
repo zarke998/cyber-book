efecb05a98c8e8dee95b88df1c33a8823e4e5eca
Andrej Zarkovski <zarke998@gmail.com>
2020-06-11 17:22:02 +0200

    Add mail notification on login fail

--- models/account/login/login_processor.php
+++ models/account/login/login_processor.php
@@ -6,6 +6,7 @@
         define("ROOT",$_SERVER["DOCUMENT_ROOT"]);
 
     require_once ROOT."/config/connection.php";
+    require_once ROOT."/models/mailer/send_mail.php";
 
     function login($email, $password, &$err_json){
         global $conn;
@@ -28,6 +29,10 @@
             $user = $stm->fetch();
 
             if(!password_verify($password, $user->password)){
+
+                $mail_body = "<h4>Failed to login to CyberBook. <br> Reason: Password incorrect. </h4>";
+                send_mail($user->email, "CyberBook - Login failed", $mail_body);
+
                 http_response_code(401);
                 $err_json = json_encode(["message" => "Password incorrect."]);
                 return false;
@@ -37,6 +42,9 @@
             return true;
         }
         catch(Exception $e){
+            $mail_body = "<h4>Failed to login to CyberBook. <br> Reason: Internal server error. </h4>";
+            send_mail($user->email, "CyberBook - Login failed", $mail_body);
+
             http_response_code(500);
             $err_json = json_encode(["message" => "Internal server error."]);
             return false;
