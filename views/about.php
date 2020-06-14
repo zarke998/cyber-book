63675ac6cfce6cdf5825ea4c7b87d4ef3b05f4f6
Andrej Zarkovski <zarke998@gmail.com>
2020-06-14 23:44:27 +0200

    Make about responsive

--- views/about.php
+++ views/about.php
@@ -1,25 +1,25 @@
 <div id="about-main" class="container-fluid px-4 my-5">
-    <div class="row">
-        <div class="col-8">
-            <div class="row">
+    <div class="row mx-0">
+        <div class="col-sm-12 offset-sm-0 offset-md-0 col-md-8">
+            <div class="row mx-0">
                 <div class="col-12 text-center mb-3">
                     <h2>About me</h2>
                 </div>
-                <div class="col-4">
+                <div class="col-8 offset-2 col-sm-6 offset-sm-3 offset-md-0 col-md-4 mb-4">
                     <img src="/assets/images/profile.jpg" alt="Profile image" class="w-100">
                 </div>
-                <div id="about-info" class="col-8">
+                <div id="about-info" class="col-12 col-md-8">
                     <p>My name is Andrej Zarkovski. I'm studying web programming at "Visoka ICT Škola" in Belgrade. In my early childhood I was very interested in how computers work and I continued to learn more about them. Now I am pursuing career in computer programming. I love japanese culture and I love playing video games.</p>
                     <div id="about-info-links">
                         <p>Portfolio: <a href="https://zarke998.github.io/web-portfolio" target="_blank">zarke998.github.io/web-portfolio</a></p>
                         <p>GitHub: <a href="https://github.com/zarke998" target="_blank">github.com/zarke998</a></p>
                         <p>LinkedIn: <a href="https://www.linkedin.com/in/andrej-zarkovski-01b8a4188" target="_blank">linkedin.com/in/andrej-zarkovski-01b8a4188</a></p>
                     </div>
-                    <a href="models/public/author_info_word.php" class="btn header-btn mx-auto d-block w-50">Download Author info</a>
+                    <a id="author-word-btn" href="models/public/author_info_word.php" class="btn header-btn mx-auto d-block w-50 mb-5">Download Author info</a>
                 </div>                
             </div>
         </div>
-        <div class="col-4">
+        <div class="col-12 col-md-4">
             <div id="about-socials mb-5">
                 <h3 class="text-center mb-3">Socials</h3>
                 <div id="about-socials-links" class="d-flex justify-content-center">
